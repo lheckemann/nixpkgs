8e0b1ed859d0badcedb00494f9b99d7f5b70918a
Linus Heckemann <git@sphalerite.org>
2017-10-12 10:05:48 +0100

    Gut module list
    This improves evaluation time of the nixos config significantly (2.6s ->
    1.7s) while preserving, as far as I can tell, all functionality I expect
    from my nixos config except for the manual, which I have consciously
    omitted because it would omit many of the available options as a result
    of this change.

--- nixos/modules/services/x11/desktop-managers/default.nix
+++ nixos/modules/services/x11/desktop-managers/default.nix
@@ -18,8 +18,7 @@ in
   # determines the default: later modules (if enabled) are preferred.
   # E.g., if Plasma 5 is enabled, it supersedes xterm.
   imports = [
-    ./none.nix ./xterm.nix ./xfce.nix ./plasma5.nix ./lumina.nix
-    ./lxqt.nix ./enlightenment.nix ./gnome3.nix ./kodi.nix ./maxx.nix
+    ./none.nix ./xterm.nix ./plasma5.nix
   ];
 
   options = {
