70b47915c6d33045f61669c58bd973cb69e88a68
Linus Heckemann <git@sphalerite.org>
2018-04-17 11:06:54 +0100

    xfce4-notes-plugin: 1.7.7 -> 1.8.1
    (cherry picked from commit a2ed886ac236fc7e74ca9fa02228f053e1e68dd1)

--- pkgs/desktops/xfce/panel-plugins/xfce4-notes-plugin.nix
+++ pkgs/desktops/xfce/panel-plugins/xfce4-notes-plugin.nix
@@ -3,23 +3,26 @@
 with stdenv.lib;
 stdenv.mkDerivation rec {
   p_name  = "xfce4-notes-plugin";
-  ver_maj = "1.7";
-  ver_min = "7";
+  ver_maj = "1.8";
+  ver_min = "1";
 
   src = fetchurl {
     url = "mirror://xfce/src/panel-plugins/${p_name}/${ver_maj}/${name}.tar.bz2";
-    sha256 = "05sjbwgch1j93m3r23ksbjnpfk11sf7xjmbb9pm5vl3snc2s3fm7";
+    sha256 = "1cjlvvcsigyh40xa26b2vc5zylgss0nlaw72sablzhii2kkw7907";
   };
   name = "${p_name}-${ver_maj}.${ver_min}";
 
+  hardeningDisable = ["format"];
+
   nativeBuildInputs = [ pkgconfig ];
   buildInputs = [ intltool libxfce4util libxfce4ui xfce4-panel libxfcegui4 xfconf gtk libunique ];
+
+  postFixup = "rm -f $out/share/icons/hicolor/icon-theme.cache";
 
   meta = {
     homepage = "http://goodies.xfce.org/projects/panel-plugins/${p_name}";
     description = "Sticky notes plugin for Xfce panel";
     platforms = platforms.linux;
     maintainers = [ maintainers.AndersonTorres ];
-    broken = true;
   };
 }
