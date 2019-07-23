3dd64b06e08967f2b5ec2ea5a19485ef885f0849
Linus Heckemann <git@sphalerite.org>
2019-07-23 08:54:06 +0200

    rPackages.V8: build with v8 7.x

--- pkgs/development/r-modules/default.nix
+++ pkgs/development/r-modules/default.nix
@@ -366,7 +366,7 @@ let
     topicmodels = [ pkgs.gsl_1 ];
     udunits2 = [ pkgs.udunits pkgs.expat ];
     units = [ pkgs.udunits ];
-    V8 = [ pkgs.v8_3_14 ];
+    V8 = [ pkgs.v8 ];
     VBLPCM = [ pkgs.gsl_1 ];
     WhopGenome = [ pkgs.zlib.dev ];
     XBRL = [ pkgs.zlib pkgs.libxml2.dev ];
@@ -856,9 +856,10 @@ let
 
     V8 = old.V8.overrideAttrs (attrs: {
       preConfigure = ''
-        export INCLUDE_DIR=${pkgs.v8_3_14}/include
-        export LIB_DIR=${pkgs.v8_3_14}/lib
+        export INCLUDE_DIR=${pkgs.v8}/include
+        export LIB_DIR=${pkgs.v8}/lib
         patchShebangs configure
+        sed -i s/-lv8_libplatform// configure
         '';
     });
 
