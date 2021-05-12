6747012009afa21fe4baf793b61bdcc9f73949ea
WilliButz <wbutz@cyberfnord.de>
2021-05-12 13:34:49 +0200

    nixos/prometheus-unbound-exporter: improve config for uds
    The group "unbound" is now added to SupplementaryGroups of the exporter
    service when accessing it locally via uds and the strict "Requires"
    option is not neccessary.

--- nixos/tests/prometheus-exporters.nix
+++ nixos/tests/prometheus-exporters.nix
@@ -1268,9 +1268,6 @@ let
           enable = true;
           localControlSocketPath = "/run/unbound/unbound.ctl";
         };
-        systemd.services.prometheus-unbound-exporter.serviceConfig = {
-          SupplementaryGroups = [ "unbound" ];
-        };
       };
       exporterTest = ''
         wait_for_unit("unbound.service")
