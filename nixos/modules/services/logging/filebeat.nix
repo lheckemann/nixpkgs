bd051216bf35c70114f9ead512c2084608607682
Robin Gloster <mail@glob.in>
2022-08-26 00:15:01 +0200

    filebeat: wtf? unremovable default -.-

--- nixos/modules/services/logging/filebeat.nix
+++ nixos/modules/services/logging/filebeat.nix
@@ -135,25 +135,6 @@ in
 
           options = {
 
-            output.elasticsearch.hosts = mkOption {
-              type = with types; listOf str;
-              default = [ "127.0.0.1:9200" ];
-              example = [ "myEShost:9200" ];
-              description = lib.mdDoc ''
-                The list of Elasticsearch nodes to connect to.
-
-                The events are distributed to these nodes in round
-                robin order. If one node becomes unreachable, the
-                event is automatically sent to another node. Each
-                Elasticsearch node can be defined as a URL or
-                IP:PORT. For example:
-                `http://192.15.3.2`,
-                `https://es.found.io:9230` or
-                `192.24.3.2:9300`. If no port is
-                specified, `9200` is used.
-              '';
-            };
-
             filebeat = {
               inputs = mkOption {
                 type = types.listOf json.type;
