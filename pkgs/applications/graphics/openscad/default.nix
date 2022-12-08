f1aff075c32ee5ea84b50a8fa51d15cf95aaf639
Linus Heckemann <git@sphalerite.org>
2022-12-08 14:33:28 +0100

    openscad: 2021.01 -> unstable-2022-12-06

--- pkgs/applications/graphics/openscad/default.nix
+++ pkgs/applications/graphics/openscad/default.nix
@@ -11,8 +11,9 @@
 , libGLU, libGL
 , glew
 , opencsg
-, cgal
+, cgal_5
 , mpfr
+, mimalloc
 , gmp
 , glib
 , pkg-config
@@ -25,7 +26,7 @@
 , libzip
 , mkDerivation
 , qtmacextras
-, qmake
+, cmake
 , spacenavSupport ? stdenv.isLinux, libspnav
 , wayland
 , wayland-protocols
@@ -35,38 +36,33 @@
 
 mkDerivation rec {
   pname = "openscad";
-  version = "2021.01";
+  version = "unstable-2022-12-06";
 
-  src = fetchFromGitHub {
+  src = builtins.fetchGit ~/projects/openscad;
+  _src = fetchFromGitHub {
     owner = "openscad";
     repo = "openscad";
-    rev = "${pname}-${version}";
-    sha256 = "sha256-2tOLqpFt5klFPxHNONnHVzBKEFWn4+ufx/MU+eYbliA=";
+    rev = "972d1846400bfce71e20cfe94683daeeed25ba1d";
+    sha256 = "sha256-xF0QuS/KI16dH6kkKHw7pbF8ppYWxDPGTTlWdJeMI+k=";
   };
 
-  patches = [
-    (fetchpatch {
-      name = "CVE-2022-0496.patch";
-      url = "https://github.com/openscad/openscad/commit/00a4692989c4e2f191525f73f24ad8727bacdf41.patch";
-      sha256 = "sha256-q3SLj2b5aM/IQ8vIDj4iVcwCajgyJ5juNV/KN35uxfI=";
-    })
-    (fetchpatch {
-      name = "CVE-2022-0497.patch";
-      url = "https://github.com/openscad/openscad/commit/84addf3c1efbd51d8ff424b7da276400bbfa1a4b.patch";
-      sha256 = "sha256-KNEVu10E2d4G2x+FJcuHo2tjD8ygMRuhUcW9NbN98bM=";
-    })
-  ];
-
-  nativeBuildInputs = [ bison flex pkg-config gettext qmake ];
+  nativeBuildInputs = [ bison flex pkg-config gettext cmake ];
 
   buildInputs = [
-    eigen boost glew opencsg cgal mpfr gmp glib
-    harfbuzz lib3mf libzip double-conversion freetype fontconfig
-    qtbase qtmultimedia qscintilla cairo
+    eigen boost glew opencsg cgal_5 mpfr gmp glib
+    harfbuzz libzip lib3mf double-conversion freetype fontconfig
+    qtbase qtmultimedia qscintilla cairo mimalloc
   ] ++ lib.optionals stdenv.isLinux [ libGLU libGL wayland wayland-protocols qtwayland ]
     ++ lib.optional stdenv.isDarwin qtmacextras
     ++ lib.optional spacenavSupport libspnav
   ;
+
+  cmakeFlags = [
+    # Tests require MCAD git submodule
+    "-DENABLE_TESTS=OFF"
+    #"-DLIB3MF_INCLUDE_DIRS=${lib.getDev lib3mf}/include/lib3mf"
+    "-DOPENSCAD_VERSION=2022.12.08"
+  ];
 
   qmakeFlags = [ "VERSION=${version}" ] ++
     lib.optionals spacenavSupport [
@@ -76,10 +72,6 @@ mkDerivation rec {
     ];
 
   enableParallelBuilding = true;
-
-  preBuild = ''
-    make objects/parser.cxx
-  '';
 
   postInstall = lib.optionalString stdenv.isDarwin ''
     mkdir $out/Applications
