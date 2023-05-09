4b8f93d7bcdcd14998a5db3ebe01007f02e4d4d3
Bjørn Forsman <bjorn.forsman@gmail.com>
2023-05-09 21:50:50 +0200

    WIP: virt-v2v: init at 2.3.4
    FIXME: `configure: error: the OCaml module 'nbd' (from libnbd) is required`
    
    https://github.com/libguestfs/virt-v2v
    
    virt-v2v used to be part of libguestfs, but was factored out into its own
    repository with the release of libguestfs 1.42.1 (see
    https://www.libguestfs.org/guestfs-release-notes-1.42.1.html).

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -35135,6 +35135,8 @@ with pkgs;
     qtermwidget = lxqt.qtermwidget;
   };
 
+  virt-v2v = callPackage ../applications/virtualization/virt-v2v { };
+
   virtscreen = callPackage ../tools/admin/virtscreen { };
 
   virtual-ans = callPackage ../applications/audio/virtual-ans { };
