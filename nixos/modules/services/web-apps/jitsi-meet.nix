273b0493a6106f44ca2f7ddecc64b3efb11626b4
Maximilian Bosch <maximilian@mbosch.me>
2023-06-05 09:55:28 +0200

    jitsi-meet: fix merge

--- nixos/modules/services/web-apps/jitsi-meet.nix
+++ nixos/modules/services/web-apps/jitsi-meet.nix
@@ -269,7 +269,6 @@ in
       };
       reloadIfChanged = true;
     };
-    systemd.services.prosody.reloadIfChanged = mkIf cfg.prosody.enable true;
 
     users.groups.jitsi-meet = {};
     systemd.tmpfiles.rules = [
