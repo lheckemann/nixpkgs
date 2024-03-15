b2551fe20ff094160c2c13d771e202e45ff7c66b
Linus Heckemann <git@sphalerite.org>
2024-03-15 08:45:57 +0100

    nixos/onlyoffice: use jsonWithRuntimeSubstitution format
    (cherry picked from commit b800ddeb5243d55efd1be5459b112f97019d9fe2)

--- nixos/modules/services/web-apps/onlyoffice.nix
+++ nixos/modules/services/web-apps/onlyoffice.nix
@@ -7,6 +7,7 @@
 
 let
   cfg = config.services.onlyoffice;
+  jsonFormat = pkgs.formats.jsonWithRuntimeSubstitution { };
 in
 {
   options.services.onlyoffice = {
@@ -112,6 +113,12 @@ in
       type = lib.types.str;
       default = "WARN";
       description = "Default loglevel to use for documentserver and converter";
+    };
+
+    settings = lib.mkOption {
+      type = lib.types.submodule {
+        freeformType = jsonFormat.type;
+      };
     };
   };
 
@@ -253,6 +260,33 @@ in
           }
         ];
       };
+
+      onlyoffice.settings.rabbitmq.url = cfg.rabbitmqUrl;
+      onlyoffice.settings.FileConverter.converter.x2tPath = "${cfg.package.x2t-with-fonts-and-themes}/bin/x2t";
+      onlyoffice.settings.services.CoAuthoring = lib.mkMerge [
+        {
+          server.port = cfg.port;
+          sql.dbHost = cfg.postgresHost;
+          sql.dbName = cfg.postgresName;
+          sql.dbPass = lib.mkIf (cfg.postgresPasswordFile != null) (
+            lib.stringFromRuntimeFile cfg.postgresPasswordFile
+          );
+          sql.dbUser = cfg.postgresUser;
+          wopi.enable = cfg.wopi;
+        }
+        (lib.mkIf (cfg.jwtSecretFile != null) {
+          token.enable.browser = true;
+          token.enable.request.inbox = true;
+          token.enable.request.outbox = true;
+          secret.inbox.string = lib.stringFromRuntimeFile cfg.jwtSecretFile;
+          secret.outbox.string = lib.stringFromRuntimeFile cfg.jwtSecretFile;
+          secret.session.string = lib.stringFromRuntimeFile cfg.jwtSecretFile;
+        })
+        (lib.mkIf (cfg.allowLocalConnections) {
+          request-filtering-agent.allowPrivateIPAddress = true;
+          request-filtering-agent.allowMetaIPAddress = true;
+        })
+      ];
     };
 
     systemd.services = {
@@ -312,41 +346,11 @@ in
 
             # for a mapping of environment variables from the docker container to json options see
             # https://github.com/ONLYOFFICE/Docker-DocumentServer/blob/master/run-document-server.sh
-            FS_SECRET_STRING=$(cut -d '"' -f 2 < ${cfg.securityNonceFile})
-            jq '
-            ${lib.optionalString cfg.allowLocalConnections ''
-              .services.CoAuthoring."request-filtering-agent".allowPrivateIPAddress = true |
-              .services.CoAuthoring."request-filtering-agent".allowMetaIPAddress = true |
-            ''}
-              .storage.fs.secretString = "'$FS_SECRET_STRING'" |
-              .services.CoAuthoring.server.port = ${toString cfg.port} |
-              .services.CoAuthoring.sql.dbHost = "${cfg.postgresHost}" |
-              .services.CoAuthoring.sql.dbName = "${cfg.postgresName}" |
-            ${lib.optionalString (cfg.postgresPasswordFile != null) ''
-              .services.CoAuthoring.sql.dbPass = "'"$(cat ${cfg.postgresPasswordFile})"'" |
-            ''}
-              .services.CoAuthoring.sql.dbUser = "${cfg.postgresUser}" |
-            ${lib.optionalString (cfg.jwtSecretFile != null) ''
-              .services.CoAuthoring.token.enable.browser = true |
-              .services.CoAuthoring.token.enable.request.inbox = true |
-              .services.CoAuthoring.token.enable.request.outbox = true |
-              .services.CoAuthoring.secret.inbox.string = "'"$(cat ${cfg.jwtSecretFile})"'" |
-              .services.CoAuthoring.secret.outbox.string = "'"$(cat ${cfg.jwtSecretFile})"'" |
-              .services.CoAuthoring.secret.session.string = "'"$(cat ${cfg.jwtSecretFile})"'" |
-            ''}
-              .rabbitmq.url = "${cfg.rabbitmqUrl}" |
-              .wopi.enable = "${toString cfg.wopi}"
-              ' /run/onlyoffice/config/default.json | sponge /run/onlyoffice/config/default.json
-
-            chmod u+w /run/onlyoffice/config/production-linux.json
-            jq '
-              .log.filePath = "/run/onlyoffice/config/log4js/production.json" |
-              .FileConverter.converter.x2tPath = "${cfg.package.x2t-with-fonts-and-themes}/bin/x2t"
-              ' /run/onlyoffice/config/production-linux.json | sponge /run/onlyoffice/config/production-linux.json
-
             chmod u+w /run/onlyoffice/config/log4js/production.json
             jq '.categories.default.level = "${cfg.loglevel}"' \
               /run/onlyoffice/config/log4js/production.json | sponge /run/onlyoffice/config/log4js/production.json
+            ${jsonFormat.generate "onlyoffice-nixos.json" cfg.settings} > /run/onlyoffice/config/nixos.json
+            jq '. * $nixos[0]' /run/onlyoffice/config/default.json.orig --slurpfile nixos /run/onlyoffice/config/nixos.json > /run/onlyoffice/config/default.json
 
             if psql -d onlyoffice -c "SELECT 'task_result'::regclass;" >/dev/null; then
               psql -f ${cfg.package}/var/www/onlyoffice/documentserver/server/schema/postgresql/removetbl.sql
